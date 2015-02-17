8e2326972f859aa5ed3bc5f8bb70fd993cb8f0ad
Erik Griffin <griffin.erik@gmail.com>
2015-02-17 14:23:15 +0000

    Playing with params

--- hello.rb
+++ hello.rb
@@ -1,6 +1,18 @@
 require 'sinatra'
 
 get '/' do
+  @name = %w(Asterix Obelix Viking).sample
+  erb :index
+end
+
+get '/hello' do
+  if params[:name] && params[:lastname]
+    @visitor = params[:name]+" "+params[:lastname]
+  else
+    if params[:name]
+      @visitor = params[:name]
+    end
+  end
   erb :index
 end
 
